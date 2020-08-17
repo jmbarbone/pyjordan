fcd20d593f4b50b0687b47f5123c4d502fe24d87
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-08-17 14:48:23 -0400

    setup with first two functions

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+# from package import Package
+
+setup(
+    name="pyjordan",
+    description="Personal function keeping for Jordan",
+    url="https://github.com/jmbarbone/pyjordan",
+    author="Jordan Mark Barbone",
+    author_email="jmbarbone@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True
+)
