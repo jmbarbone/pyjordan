cbef3dcbdb3ca6a023d1b58903fb4adf1b68ce6a
Jordan Mark Barbone <38573843+jmbarbone@users.noreply.github.com>
2020-08-21 09:36:05 -0400

    adds a replace_all function

--- pyjordan/__init__.py
+++ pyjordan/__init__.py
@@ -59,3 +59,32 @@ def find_file_recursive(path, pattern, tries=100):
 
     return found_file
     
+
+def replace_all(text, dic):
+    """ Dictionary replacements
+
+    Replaces strings found in dictionary
+    
+    ## Keyword arguments: 
+    text -- The text to replace
+    dic -- The dictionary of items to replace
+
+    ## Reference 
+    https://stackoverflow.com/a/6117042
+
+    ## *WARNINGS* 
+    Python dictionaries don't have a reliable order for iteration. This solution only solves your problem if: 
+    * order of replacements is irrelevant
+    * it's ok for a replacement to change the results of previous replacements
+
+    Inefficient if `text` is too big or there are many pairs in the dictionary.
+    """
+
+    from collections ipmort OrderdDict
+
+    dic = OrderedDict(dic)
+
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    
+    return text
