3599f1647d6b97a7df8bf5ede40cbf51f60e1300
Jordan Mark Barbone <jmbarbone@gmail.com>
2021-01-12 23:06:00 -0500

    documentation

--- pyjordan/strings.py
+++ pyjordan/strings.py
@@ -3,19 +3,30 @@ def str_replace_all(text, dic):
 
     Replaces strings found in dictionary
 
-    ## Keyword arguments:
-    text -- The text to replace
-    dic -- The dictionary of items to replace
+    Parameters
+    ----------
+    text : string
+        The text to replace
+    dic : dictionary
+        The dictionary of items to replace
 
-    ## Reference
+    Reference
+    ---------
     https://stackoverflow.com/a/6117042
 
-    ## *WARNINGS*
+    Warnings
+    --------
     Python dictionaries don't have a reliable order for iteration. This solution only solves your problem if:
     * order of replacements is irrelevant
     * it's ok for a replacement to change the results of previous replacements
 
     Inefficient if `text` is too big or there are many pairs in the dictionary.
+
+    Example
+    -------
+    x = "The quick brown fox"
+    d = {'brown': 'green', 'quick': 'slow', 'fox': 'platypus'}
+    str_replace_all(x, d)
     """
 
     from collections import OrderedDict
