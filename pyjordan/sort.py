bbdcb293f3d145de614c0d5644d60431b0c2b60b
jmbarbone <jmbarbone@gmail.com>
2022-02-17 23:00:42 -0500

    Create sort.py

---
+++ pyjordan/sort.py
@@ -0,0 +1,23 @@
+
+def sort_by(x, y) :
+    """
+    Sort x by y
+
+    Description
+    -----------
+    Sort values in one list by values in another
+
+    Parameters
+    ----------
+    x : list
+        A list of values
+    y : list
+        A list of values
+    
+    Returns
+    -------
+    res : list
+        Values of x sorted by values of y
+    """
+    res = [x for _, x in sorted(zip(y, x))]
+    return res
