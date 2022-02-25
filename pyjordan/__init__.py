082790679d7cc91380bccd5b44908288b2e423bc
jmbarbone <jmbarbone@gmail.com>
2022-02-24 22:43:37 -0500

    export them; formatting

--- pyjordan/__init__.py
+++ pyjordan/__init__.py
@@ -1,3 +1,12 @@
 from pyjordan.files import find_file, find_file_recursive
 from pyjordan.strings import str_replace_all
-from pyjordan.tools import print_time, round_by, unnest, as_list
+from pyjordan.tools import (
+    print_time,
+    round_by,
+    unnest,
+    as_list,
+    is_windows,
+    is_linux,
+    is_mac
+)
+    
